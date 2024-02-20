b945b4786cb1a66c25a5d1a3a01a73da29f41c9f
Chase Hutcheson <111781666+ChaseHutcheson@users.noreply.github.com>
2024-02-20 14:09:34 -0500

    Working on basic data types

---
+++ 01_Basic_Data_Types/numbers.rb
@@ -0,0 +1,24 @@
+# Addition
+print "1 + 1 = ", 1 + 1, "\n"
+
+# Subtraction
+print "1 - 1 = ", 1 - 1, "\n"
+
+# Multiplication
+print "1 * 1 = ", 1 * 1, "\n"
+
+# Division
+print "1 / 1 = ", 1 / 1, "\n"
+
+# Exponents
+print "2 ** 4 = ", 2 ** 4, "\n"
+
+# Modulus / Modulo
+print "8 % 3 = ", 8 % 3, "\n"
+
+# Integers and Floats *NOTE: Arithmatic always results in Integers
+# Integer
+print "Integer (Whole Number): ", 17, "\n"
+
+# Float
+print "Float (Decimal): ", 17.9, "\n"
